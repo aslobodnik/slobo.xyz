c1eb38c235bb1a2b0d8b0fce83a15ac7d3bf2eb5
aslobodnik <aslobodnik@gmail.com>
2024-07-25 13:49:05 -0400

    add invisible footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js,njk,md}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionDuration: {
+        3000: "3000ms",
+      },
+    },
   },
   plugins: [],
 };
